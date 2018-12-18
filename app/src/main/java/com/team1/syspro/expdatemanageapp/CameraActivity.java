dd346f16a081a713a5ddf959549a6b8b036277fa
imura-lab(mac) <ishizuka@cyb.sc.e.titech.ac.jp>
2018-12-18 16:35:19 +0900

    #15 SDK API>=23からストレージの保存やカメラの起動に，ユーザーからのpermissionが必要になった． それに対応した．

--- app/src/main/java/com/team1/syspro/expdatemanageapp/CameraActivity.java
+++ app/src/main/java/com/team1/syspro/expdatemanageapp/CameraActivity.java
@@ -2,10 +2,13 @@ package com.team1.syspro.expdatemanageapp;
 
 import android.content.ContentResolver;
 import android.content.ContentValues;
+import android.content.Intent;
 import android.hardware.Camera;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MotionEvent;
@@ -17,58 +20,67 @@ import android.view.View;
 import java.io.FileOutputStream;
 
 public class CameraActivity extends AppCompatActivity {
+    private boolean isStragePermission;
     private Camera m_Camera;
     private SurfaceView m_SurfaceView;
     private SurfaceHolder.Callback m_SurfaceListener =
             new SurfaceHolder.Callback() {
                 @Override
                 public void surfaceCreated(SurfaceHolder holder) {
-
-                    m_Camera = Camera.open();
-                    try{
-                        m_Camera.setPreviewDisplay(holder);
-                    }catch (Exception e){
-                        e.printStackTrace();
-                    }
+                        m_Camera = Camera.open();
+                        try {
+                            m_Camera.setPreviewDisplay(holder);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
                 }
 
                 @Override
                 public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-                    Camera.Parameters parameters = m_Camera.getParameters();
-                    //parameters.setRotation(180);
+                        Camera.Parameters parameters = m_Camera.getParameters();
+                        //parameters.setRotation(180);
 
-                    int cameraId = 0;
-                    android.hardware.Camera.CameraInfo info =
-                            new android.hardware.Camera.CameraInfo();
-                    android.hardware.Camera.getCameraInfo(cameraId, info);
-                    int rotation = getWindowManager().getDefaultDisplay()
-                            .getRotation();
-                    int degrees = 0;
-                    switch (rotation) {
-                        case Surface.ROTATION_0: degrees = 0; break;
-                        case Surface.ROTATION_90: degrees = 90; break;
-                        case Surface.ROTATION_180: degrees = 180; break;
-                        case Surface.ROTATION_270: degrees = 270; break;
+                        int cameraId = 0;
+                        android.hardware.Camera.CameraInfo info =
+                                new android.hardware.Camera.CameraInfo();
+                        android.hardware.Camera.getCameraInfo(cameraId, info);
+                        int rotation = getWindowManager().getDefaultDisplay()
+                                .getRotation();
+                        int degrees = 0;
+                        switch (rotation) {
+                            case Surface.ROTATION_0:
+                                degrees = 0;
+                                break;
+                            case Surface.ROTATION_90:
+                                degrees = 90;
+                                break;
+                            case Surface.ROTATION_180:
+                                degrees = 180;
+                                break;
+                            case Surface.ROTATION_270:
+                                degrees = 270;
+                                break;
+                        }
+
+                        int result;
+                        if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
+                            result = (info.orientation + degrees) % 360;
+                            result = (360 - result) % 360;  // compensate the mirror
+                        } else {  // back-facing
+                            result = (info.orientation - degrees + 360) % 360;
+                        }
+                        m_Camera.setDisplayOrientation(result);
+                        parameters.setPreviewSize(height, width);// here w h are reversed
+
+                        m_Camera.setParameters(parameters);
+                        m_Camera.startPreview();
                     }
-
-                    int result;
-                    if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
-                        result = (info.orientation + degrees) % 360;
-                        result = (360 - result) % 360;  // compensate the mirror
-                    } else {  // back-facing
-                        result = (info.orientation - degrees + 360) % 360;
-                    }
-                    m_Camera.setDisplayOrientation(result);
-                    parameters.setPreviewSize(height, width);// here w h are reversed
-
-                    m_Camera.setParameters(parameters);
-                    m_Camera.startPreview();
-                }
 
                 @Override
                 public void surfaceDestroyed(SurfaceHolder holder) {
-                    m_Camera.release();
-                    m_Camera = null;
+                        m_Camera.release();
+                        m_Camera = null;
+
                 }
             };
     // シャッターが押されたときに呼ばれるコールバック
@@ -85,13 +97,15 @@ public class CameraActivity extends AppCompatActivity {
             if (data != null) {
                 FileOutputStream fos = null;
                 try {
-                    String filepath = Environment.getExternalStorageDirectory().getPath()+ "/camera_test.jpg";
-                    fos = new FileOutputStream(filepath);
-                    Log.d("my-debug",filepath);
-                    fos.write(data);
-                    fos.close();
-                    // 保存した画像をアンドロイドのデータベースへ登録(一時的な処理)
-                    registerDatabase(filepath);
+                    if(isStragePermission) {
+                        String filepath = Environment.getExternalStorageDirectory().getPath() + "/camera_test.jpg";
+                        fos = new FileOutputStream(filepath);
+                        Log.d("my-debug", filepath);
+                        fos.write(data);
+                        fos.close();
+                        // 保存した画像をアンドロイドのデータベースへ登録(一時的な処理)
+                        registerDatabase(filepath);
+                    }
                 } catch (Exception e) {
                     Log.e("my-debug", "message", e);
                 }
@@ -132,15 +146,24 @@ public class CameraActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
         Log.d("my-debug", "camera- onCreate");
+        isStragePermission =  getIntent().getBooleanExtra("isStoragePermission",false);
 
-        m_SurfaceView = (SurfaceView)findViewById(R.id.surface_view);
+        m_SurfaceView = (SurfaceView) findViewById(R.id.surface_view);
         SurfaceHolder holder = m_SurfaceView.getHolder();
         // surfaceholderにsurfacelistenerを設定
         holder.addCallback(m_SurfaceListener);
         View view = findViewById(R.id.content);
         // touch時の挙動を設定
         m_SurfaceView.setOnTouchListener(m_ontouchListener);
-        //:TODO カメラ起動時にはパーミッションをうんたらしないといけないらしい．
-        //holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
+    }
+
+
+    private void openSettings() {
+        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        //Fragmentの場合はgetContext().getPackageName()
+        Uri uri = Uri.fromParts("package", getPackageName(), null);
+        intent.setData(uri);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/team1/syspro/expdatemanageapp/MainActivity.java
+++ app/src/main/java/com/team1/syspro/expdatemanageapp/MainActivity.java
@@ -1,12 +1,23 @@
 package com.team1.syspro.expdatemanageapp;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.PermissionChecker;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private int REQUEST_CODE_PERMISSIONS= 0x11;
+    private boolean isCameraPermission;
+    private boolean isStragePermission;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,9 +38,101 @@ public class MainActivity extends AppCompatActivity {
         tempButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplication(), CameraActivity.class);
-                startActivity(intent);
+                isCameraPermission = false;
+                isStragePermission = false;
+                if (Build.VERSION.SDK_INT >= 23) {
+                    // camera permission
+                    if ((PermissionChecker.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) ||
+                            (PermissionChecker.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)) {
+                        Log.d("my-debug", "API >= 23, request camera and storage permission .");
+                        requestPermissions();
+                    }else{
+                        isCameraPermission = true;
+                        isStragePermission = true;
+                        Log.d("my-debug", "API >= 23, camera and storage permissions: GRANTED.");
+                        Intent intent = new Intent(getApplication(), CameraActivity.class);
+                        intent.putExtra("isStoragePermission",isStragePermission);
+                        startActivity(intent);
+                    }
+                } else {
+                    isCameraPermission = true;
+                    isStragePermission = true;
+                    //startSetting();
+                    Log.d("my-debug", "API <= 23 .");
+                    //API 23以下なので許可なく起動
+                    Intent intent = new Intent(getApplication(), CameraActivity.class);
+                    intent.putExtra("isStoragePermission",isStragePermission);
+                    startActivity(intent);
+                }
             }
         });
     }
+
+
+    void requestPermissions() {
+        // 権限を要求する
+        ActivityCompat.requestPermissions(this, new String[]{
+                        Manifest.permission.CAMERA,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE
+                },
+                REQUEST_CODE_PERMISSIONS);
+    }
+
+    // userがバーミッションをどうしたかが帰ってくる
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        if (requestCode == REQUEST_CODE_PERMISSIONS && grantResults.length == 2) {
+            for (int i=0;i<2;i++){
+                if(permissions[i].equals(Manifest.permission.CAMERA)){
+                    if(grantResults[i] != PackageManager.PERMISSION_GRANTED){
+                        Log.d("my-debug","Request Camera Permission : DENIED");
+                        new AlertDialog.Builder(this)
+                                .setTitle("パーミッション取得エラー")
+                                .setMessage("カメラは起動しません")
+                                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int i) {
+                                        // サンプルのため、今回はもう一度操作をはさんでいますが
+                                        // ここでrequestCameraPermissionメソッドの実行でもよい
+                                    }
+                                })
+                                .create()
+                                .show();
+                    }else{
+                        isCameraPermission = true;
+                    }
+                }
+                if(permissions[i].equals(Manifest.permission.WRITE_EXTERNAL_STORAGE)){
+                    if(grantResults[i] != PackageManager.PERMISSION_GRANTED){
+                        Log.d("my-debug","Request Storage Permission : DENIED");
+                        new AlertDialog.Builder(this)
+                                .setTitle("パーミッション取得エラー")
+                                .setMessage("ストレージの書き込みはできません")
+                                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int i) {
+                                        // サンプルのため、今回はもう一度操作をはさんでいますが
+                                        // ここでrequestCameraPermissionメソッドの実行でもよい
+                                    }
+                                })
+                                .create()
+                                .show();
+                    }else{
+                        isStragePermission = true;
+                    }
+                }
+            }
+            // camera permissionがない場合はCameraActivityを起動しない．
+            if(isCameraPermission) {
+                Intent intent = new Intent(getApplication(), CameraActivity.class);
+                intent.putExtra("isStoragePermission",isStragePermission);
+                startActivity(intent);
+            }
+            //startSetting();
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
 }
